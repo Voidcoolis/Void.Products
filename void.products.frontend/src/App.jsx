9f08351711c720a15aed34d03eb35d9b805375e7
Bonjov Hima <bonjovhima@hotmail.de>
2025-06-07 16:44:20 +0200

    added Navbar component & configured CreateProductPage

--- void.products.frontend/src/App.jsx
+++ void.products.frontend/src/App.jsx
@@ -1,10 +1,20 @@
-import { Button } from '@chakra-ui/react'
+import { Box, useColorModeValue } from '@chakra-ui/react';
+import { Route, Routes } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import Navbar from './components/Navbar';
+import CreateProductPage from './pages/CreateProductPage';
 
 function App() {
 
   return (
     <>
-      <Button>Hello Chakra</Button>
+      <Box minH={"100vh"} bg={useColorModeValue("snow", "gray.900")}>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<HomePage/>} />
+          <Route path='/create' element={<CreateProductPage />} />
+        </Routes>
+      </Box>
     </>
   )
 }
